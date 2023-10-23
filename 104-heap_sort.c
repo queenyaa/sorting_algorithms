2693ef373aaa506670da6550c8716f15fdb59744
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-10-23 07:47:12 -0700

    heap

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,49 +1,45 @@
 #include "sort.h"
 
 /**
- * heapify - Heapify a heap node
- * @array: array
- * @n: array size
- * @x: root index
- * @size: array size
+ * swap_heap - swap array
+ * @arr: pointer to the first int
+ * @siz: pointer to the second int
  */
-void heapify(int *array, size_t n, size_t x, size_t size)
+void swap_heap(int *arr, int *siz)
 {
-	size_t largest, left, right;
 	int temp;
 
-	while ((left = (2 * n) + 1) <= size)
-	{
-		largest = n;
-		right = left + 1;
-		if (array[largest] < array[left])
-			largest = left;
-		if (right <= x && array[largest] < array[right])
-			largest = right;
-		if (largest == n)
-			return;
-		temp = array[n];
-		array[n] = array[largest];
-		array[largest] = temp;
-		print_array(array, size);
-		n = largest;
-	}
+	temp = *arr;
+	*arr = *siz;
+	*siz = temp;
 }
 
 /**
- * heapitol - makes a heap from unsorted array
- * @array: array to turn into heap
- * @size: size of the array
+ * heapify - Heapify a heap node
+ * @array: array
+ * @n: base row of the tree
+ * @x: root index
+ * @size: array size
  */
-void heapitol(int *array, size_t size)
+void heapify(int *array, size_t size, size_t n, size_t x)
 {
-	size_t upper;
+	size_t largest, left, right;
 
-	for (upper = ((size - 1) - 1) / 2; upper != 0; upper--)
+	left = 2 * x + 1;
+	right = 2 * x + 2;
+	largest = x;
+
+	if (left < n && array[left] > array[largest])
+		largest = left;
+	if (right < n && array[right] > array[largest])
+		largest = right;
+
+	if (largest != x)
 	{
-		heapify(array, upper, size - 1, size);
+		swap_heap(array + x, array + largest);
+		print_array(array, size);
+		heapify(array, size, n, largest);
 	}
-
 }
 
 /**
@@ -53,20 +49,18 @@ void heapitol(int *array, size_t size)
  */
 void heap_sort(int *array, size_t size)
 {
-        size_t end;
-        int temp;
+	int w;
 
-        if (array == NULL || size < 2)
-                return;
-	heapitol(array, size);
-	end = size - 1;
-	while (end > 0)
+	if (array == NULL || size < 2)
+		return;
+
+	for (w = (size / 2) - 1; w >= 0; w--)
+		heapify(array, size, size, w);
+
+	for (w = size - 1; w > 0; w--)
 	{
-		temp = array[end];
-		array[end] = array[0];
-		array[0] = temp;
+		swap_heap(array, array + w);
 		print_array(array, size);
-		end--;
-		heapify(array, 0, end, size);
+		heapify(array, size, w, 0);
 	}
 }
